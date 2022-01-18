2f1048147aaaf1464a88b14b64e8dcdc2b54aaf5
Angelgroenen <bm.bracke.michael@gmail.com>
2022-01-18 16:49:39 +0100

    second part of form exercise

--- Forms/script.js
+++ Forms/script.js
@@ -5,9 +5,28 @@ console.log(document.forms);
 
 const handleForm = (event) => {
     event.preventDefault ();
-    let newName = event.target.value
+    let newName = event.target.value;
     console.log(event.target.value);
     display.innerHTML = newName;
 }
 
 form.addEventListener ('keyup', handleForm);
+
+
+
+const form2 = document.querySelector('#age');
+const display2 = document.querySelector('#a-hard-truth');
+
+const handleFormAge = (event) => {
+    event.preventDefault;
+    let newAge = event.target.value;
+    console.log(newAge);
+   if (newAge < 18) {
+        display2.style.visibility = "hidden";
+   } else {
+        display2.style.visibility = "visible";
+    }
+   }
+
+
+form2.addEventListener('keyup', handleFormAge);
